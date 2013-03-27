965c82a6aa452f0934b4851948a4ff7d4598904a
Pippin Williamson <pippin@pippinsplugins.com>
2013-03-27 15:21:00 -0500

    First pass at CGC_Profile_Stats_Followers

--- includes/class-stats-followers.php
+++ includes/class-stats-followers.php
@@ -2,4 +2,16 @@
 
 class CGC_Profile_Stats_Followers extends CGC_Profile_Stats_Base {
 
+	private function init() {
+		$this->type = 'followers';
+	}
+
+	private function query() {
+
+		if( function_exists( 'cgc_get_following_count' ) )
+			return cgc_get_following_count( $this->user_id );
+		else
+			return 0;
+	}
+
 }
