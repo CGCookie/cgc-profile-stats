d7580877466a4366c61916fa599ca871ac1d0309
Pippin Williamson <pippin@pippinsplugins.com>
2013-03-27 14:18:16 -0500

    Initial likes class

--- includes/class-stats-likes.php
+++ includes/class-stats-likes.php
@@ -2,4 +2,41 @@
 
 class CGC_Profile_Stats_Likes extends CGC_Profile_Stats_Base {
 
+	private function init() {
+		$this->type = 'likes';
+	}
+
+	public function get_data( $user_id = 0 ) {
+		return $this->query( array( 'author' => $user_id ) );
+	}
+
+	private function query( $args = array() ) {
+
+		$defaults = array(
+			'author'    => 0,
+			'post_type' => 'images',
+			'nopaging'  => true,
+			'fields'    => 'ids',
+			'update_post_meta_cache' => false,
+			'update_post_term_cache' => false
+		);
+
+		$args   = wp_parse_args( $args, $defaults );
+
+		$images = get_posts( $args );
+
+		$likes  = 0;
+
+		if( $images ) :
+			foreach( $images as $image ) :
+				$count = get_post_meta( $image, '_cgc_love_count', true );
+				if( is_int( $count ) )
+					$likes += $count;
+			endforeach;
+		endif;
+
+		return $likes;
+
+	}
+
 }
