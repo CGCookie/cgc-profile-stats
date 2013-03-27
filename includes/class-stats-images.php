e3d4d9b95e481aefa5182b3dbe21501b2a8d12fa
Pippin Williamson <pippin@pippinsplugins.com>
2013-03-27 14:13:43 -0500

    Initia sub class for image stats

--- includes/class-stats-images.php
+++ includes/class-stats-images.php
@@ -2,4 +2,31 @@
 
 class CGC_Profile_Stats_Images extends CGC_Profile_Stats_Base {
 
+	private function init() {
+		$this->type = 'images';
+	}
+
+	public function get_data( $user_id = 0 ) {
+		return $this->query( array( 'author' => $user_id ) );
+	}
+
+	private function query( $args = array() ) {
+
+		$defaults = array(
+			'author'    => 0,
+			'post_type' => 'images',
+			'nopaging'  => true,
+			'fields'    => 'ids',
+			'update_post_meta_cache' => false,
+			'update_post_term_cache' => false
+		);
+
+		$args  = wp_parse_args( $args, $defaults );
+
+		$query = new WP_Query( $args );
+
+		return $query->post_count;
+
+	}
+
 }
