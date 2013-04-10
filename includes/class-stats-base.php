5abf3b1c280d292bb894bfa5f62eafaa816fa9c9
Pippin Williamson <pippin@pippinsplugins.com>
2013-04-10 11:57:21 -0500

    Always refresh stats

--- includes/class-stats-base.php
+++ includes/class-stats-base.php
@@ -38,8 +38,8 @@ class CGC_Profile_Stats_Base {
 		// Check if stats need to be refreshed
 		//if( ! isset( $stats['modified'] ) || $stats['modified'] < strtotime( '-1 day' ) ) {
 		if( ! isset( $stats['modified'] ) || $stats['modified'] < strtotime( '-1 minute' ) ) {
-			$this->refresh_stats();
 		}
+			$this->refresh_stats();
 
 		return $this->stats;
 	}
